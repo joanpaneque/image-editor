26c20376774b7791587846553d82bd819b5bf584
Joan Paneque <85831928+joanpaneque@users.noreply.github.com>
2022-06-21 12:07:29 +0200

    Delete image-editor directory

--- image-editor/image-editor/dom/dom.js
+++
@@ -1,8 +0,0 @@
-export var canvas = document.getElementById('canvas');
-
-// Toolbox
-export var tb = document.getElementById('toolbox');
-export var tb_container = document.getElementById("tools");
-
-
-export var as = document.getElementById('aside');
